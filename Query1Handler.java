720d3bf5df90558ebf0689e9cb720be8caf6ef53
parthmittal <parth15069@iiitd.ac.in>
2016-11-29 17:23:07 +0530

    great refactoring event

--- Query1Handler.java
+++ Query1Handler.java
@@ -7,11 +7,13 @@ public class Query1Handler extends DefaultHandler {
     protected static final Set<String> PUB_TYPES = new HashSet<>(Arrays.asList("article", "book", "incollection", "inproceedings", "phdthesis", "proceedings", "mastersthesis"));
     protected boolean store;
     protected String buffer;
+    protected ArrayList<Pair<Integer, Publication>> ret;
 
     public void startDocument() throws SAXException {
         currentPublication = null;
         store = false;
         buffer = new String();
+        ret = new ArrayList<>();
     }
 
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
@@ -21,9 +23,46 @@ public class Query1Handler extends DefaultHandler {
         }
     }
 
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (!PUB_TYPES.contains(localName) && store) {
+            switch (localName) {
+                case "author":
+                    currentPublication.addAuthor(buffer);
+                    break;
+                case "booktitle":
+                    currentPublication.setBookTitle(buffer);
+                    break;
+                case "journal":
+                    currentPublication.setJournal(buffer);
+                    break;
+                case "pages":
+                    currentPublication.setPages(buffer);
+                    break;
+                case "title":
+                    currentPublication.setTitle(buffer);
+                    break;
+                case "url":
+                    currentPublication.setUrl(buffer);
+                    break;
+                case "volume":
+                    currentPublication.setVolume(buffer);
+                    break;
+                case "year":
+                    currentPublication.setYear(Integer.parseInt(buffer));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     public void characters(char[] ch, int start, int length) throws SAXException {
         if (store) {
             buffer += new String(ch, start, length);
         }
+    }
+
+    public ArrayList<Pair<Integer, Publication>> getRet() {
+        return ret;
     }
 }
